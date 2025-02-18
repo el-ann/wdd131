51f6b74f733d0c36ac8db7aff9db8058455cecd4
Elizabeth Ann Adjabeng <ewuradwoaann@gmail.com>
2025-02-18 23:39:12 +0200

    product form review commit

---
+++ scripts/review.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let reviewCount = localStorage.getItem("reviewCount") || 0;
+    reviewCount++;
+    localStorage.setItem("reviewCount", reviewCount);
+    document.getElementById("reviewCount").textContent = reviewCount;
+    
+    
+});
+
+
